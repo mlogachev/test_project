db071d0b7a57c0c5af20c28ebfbda181e1e39dd3
Mikhail Logachev <miha.loga@gmail.com>
2018-10-30 15:22:20 +0300

    backend, routes for getting, updating, fetching statuses

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -12,4 +12,6 @@ from base import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^get/$', views.GetTasksView.as_view(), name='get_tasks'),
+    url(r'^statuses/get$', views.GetTaskStatusesView.as_view(), name='get_tasks_statuses'),
+    url(r'^update/$', views.UpdateTask.as_view(), name='update_task'),
 ]
--- apps/base/views.py
+++ apps/base/views.py
@@ -5,10 +5,12 @@ from __future__ import (
 )
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
+from django.db.models import Q
+from django.http import JsonResponse, QueryDict
 from django.views import View
 from django.views.generic import TemplateView
 
+from .models import Task, TaskStatus
 
 __all__ = (
     b'IndexView',
@@ -22,6 +24,43 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
 class GetTasksView(LoginRequiredMixin, View):
 
-    def get(self, *args, **kwargs):
-        items = []
-        return JsonResponse({'items': items})
+    def get(self, request, *args, **kwargs):
+        visible_tasks = list(
+            Task.objects.filter(
+                Q(status=TaskStatus.OPEN) |
+                Q(status__range=[TaskStatus.IN_PROGRESS, TaskStatus.FINISHED], assignee_id=request.user.id)
+            )
+            .order_by('-id')
+            .values()
+        )
+
+        return JsonResponse(visible_tasks, safe=False)
+
+
+class GetTaskStatusesView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({
+            choice_pair[0]: choice_pair[1]
+            for choice_pair in TaskStatus.CHOICES
+        })
+
+
+class UpdateTask(LoginRequiredMixin, View):
+    def put(self, request, *args, **kwargs):
+        qd = QueryDict(request.body)
+        task_id = qd.get('id')
+        ui_status = int(qd.get('status'))
+
+        task = Task.objects.get(id=task_id)
+        if not task:
+            return JsonResponse({"success": False, "message": "Task not found"}, status=404)
+
+        task_status = task.status
+        if task_status != ui_status:
+            return JsonResponse({"success": False, "message": "Data mismatch error"}, status=409)
+
+        task.status = TaskStatus.next_status(task_status)
+        task.assignee_id = request.user.id
+        task.save()
+
+        return JsonResponse({"success": True, "status": task.status}, status=200)
