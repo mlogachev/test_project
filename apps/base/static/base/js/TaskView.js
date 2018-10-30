d7d82adf3f611b38bd334c85cabf121a09268798
Mikhail Logachev <miha.loga@gmail.com>
2018-10-30 15:23:07 +0300

    task template, handling of ui events

--- apps/base/static/base/js/TaskView.js
+++ apps/base/static/base/js/TaskView.js
@@ -6,7 +6,43 @@
 
 
     ns.views.TaskView = Backbone.Marionette.ItemView.extend({
-        template: '#task-tpl'
+        template: '#task-tpl',
+        className: 'panel panel-default',
+        events: {
+            'click .action-button': 'actionButtonClicked',
+        },
+
+        initialize: function(options) {
+            console.log(options);
+            if (options) {
+                this.updateRoute = options.updateRoute;
+            }
+        },
+
+        actionButtonClicked: function (event) {
+            let self = this;
+            let currentId = this.model.get('id'),
+                currentStatus = this.model.get('status');
+            console.log(this.updateRoute);
+            console.log(currentId);
+            $.ajax({
+                url: this.updateRoute,
+                method: "PUT",
+                data: {
+                    CSRF: ns.csrf,
+                    id: currentId,
+                    status: currentStatus,
+                },
+                success: function () {
+                    self.trigger('reRender', self);
+                },
+                error: function(err) {
+                    self.$el.append("<div class='alert alert-warning'> Статус задания изменен </div>");
+                    setTimeout(() => self.trigger('reRender', self), 3000);
+                    self.trigger('failed', self, err);
+                }
+            })
+        }
     });
 
 })(App);
--- apps/base/static/base/js/models.js
+++ apps/base/static/base/js/models.js
@@ -5,12 +5,11 @@
     ns.models = ns.models || {};
     ns.collections = ns.collections || {};
 
-
     ns.models.Task = Backbone.Model.extend({});
 
 
     ns.collections.Tasks = Backbone.Collection.extend({
-        model: ns.models.Task
+        model: ns.models.Task,
     });
 
 
