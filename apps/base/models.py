f6d6e2f76f081e5ab76734c7b4dc39bf2edaaf53
Mikhail Logachev <miha.loga@gmail.com>
2018-10-30 15:24:55 +0300

    model update, migration, convenient methods

--- apps/base/models.py
+++ apps/base/models.py
@@ -9,25 +9,29 @@ from django.db import models
 
 
 class TaskStatus(object):
-
     OPEN = 1
     IN_PROGRESS = 20
     FINISHED = 30
 
     CHOICES = (
-        (OPEN, 'OPEN'),
-        (IN_PROGRESS, 'IN_PROGRESS'),
-        (FINISHED, 'FINISHED'),
+        (OPEN, u'Ожидается'),
+        (IN_PROGRESS, u'Принято'),
+        (FINISHED, u'Выполнено'),
     )
+
+    @classmethod
+    def next_status(cls, task_status):
+        if task_status == TaskStatus.OPEN:
+            return TaskStatus.IN_PROGRESS
+        else:
+            return TaskStatus.FINISHED
 
 
 class Task(models.Model):
-
     text = models.TextField()
     link = models.URLField(max_length=1024)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='q', null=False, blank=False, default=1)
     assignee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tasks', null=True, blank=True)
     status = models.SmallIntegerField(choices=TaskStatus.CHOICES, default=TaskStatus.OPEN)
-
-
